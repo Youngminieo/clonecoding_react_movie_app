8651fdb5f32f41ec2642a624482bf5b06fd5f034
WI Young Min <youngminieo1005@gmail.com>
2021-02-08 23:52:01 +0900

    JSX props Part 2

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
-import Potato from "./Potato";
+
+// 인자로, props의 이름을 직접 받아도 되고
+// props로 뭉탱이로 넘겨도 된다.
+function Potato({favorite}){
+  return(
+    <h1>I like {favorite}</h1>
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Hello!!</h1>
-      <Potato />
+      <Potato favorite="Kimchi" />
+      <Potato favorite="rice" />
+      <Potato favorite="lamen" />
     </div>
   );
 }

a338736ef41268863660fc14ba3204d39eca5220
WI Young Min <youngminieo1005@gmail.com>
2021-02-10 02:19:22 +0900

    Fetch movies from api part 1

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from 'prop-types';
+import axios from "axios";
 
 class App extends React.Component{
   state = {
@@ -7,11 +8,12 @@ class App extends React.Component{
     movies : []
   };
 
+  getMovies = async() => {
+    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+  };
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
-  }
+    this.getMovies();
+  };
 
   render(){
     const { isLoading } = this.state;

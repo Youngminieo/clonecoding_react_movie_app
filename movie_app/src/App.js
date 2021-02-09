b0193e8dbbd9ab167c5d1826eb265b6382dabc73
WI Young Min <youngminieo1005@gmail.com>
2021-02-10 02:50:05 +0900

    rendering the movies

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component{
   state = {
@@ -8,20 +9,32 @@ class App extends React.Component{
     movies : []
   };
 
-  getMovies = async() => {
-    const movies = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+  getMovies = async () => {
+    const { data: { data: { movies } } } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json");
+    this.setState({movies, isLoading:false});
   };
   componentDidMount(){
     this.getMovies();
   };
 
   render(){
-    const { isLoading } = this.state;
+    const { isLoading, movies } = this.state;
     return(
       <div>
-        {isLoading ? "Loading..." : "We are ready !!"}
+        {isLoading 
+          ? "Loading..." 
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                />
+          ))}
       </div>
-    )
+    );
   }
 }
 export default App;
